66aaa7a529cf7e390528efb76544b87d692d8095
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2009-09-14 17:41:09 +0000

    created main structure and added some first files to download historical grib files
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@1 c5ce1916-e146-4858-a6ec-1f747f202df1

---
+++ grib/download_old_grib_files.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# simple script to download 4x daily V winds at 10mb
+# from the R2 archive
+
+set -x
+date=200401
+enddate=200908
+while [ $date -le $enddate ]
+do
+     url="http://nomad3.ncep.noaa.gov/pub/reanalysis-2/6hr/pgb/pgb.$date"
+     ./get_inv.pl "${url}.inv" | grep ":VGRD:" | grep ":10 mb" | \
+     ./get_grib.pl "${url}" pgb.$date
+     date=$(($date + 1))
+     if [ $(($date % 100)) -eq 13 ] ; then
+         date=$(($date - 12 + 100));
+     fi
+done
