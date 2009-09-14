b7aa8de8f632307f830d86f5fda99fe6b0ebb648
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2009-09-14 19:34:04 +0000

    - creating subdirectories for the grib files downloaded by the scripts - sing a svn:externals for the wdb, a weather database system
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@3 c5ce1916-e146-4858-a6ec-1f747f202df1

--- grib/download_forecast_grib_file.sh
+++ grib/download_forecast_grib_file.sh
@@ -3,4 +3,6 @@
 
 cdate=$(date +%04Y%02m%02d)00
 echo $cdate
-./get_gfs.pl data $cdate 0 72 3 HGT:TMP surface:850_mb:750_mb:500_mb . 
+mkdir ./present
+rm -r ./present/*
+./get_gfs.pl data $cdate 0 72 3 HGT:TMP surface:850_mb:750_mb:500_mb ./present 
--- grib/download_old_grib_files.sh
+++ grib/download_old_grib_files.sh
@@ -2,14 +2,18 @@
 # simple script to download 4x daily V winds at 10mb
 # from the R2 archive
 
+mkdir ./past
+rm -r ./past/*
+cd ./past
+
 set -x
 date=200401
 enddate=200908
 while [ $date -le $enddate ]
 do
      url="http://nomad3.ncep.noaa.gov/pub/reanalysis-2/6hr/pgb/pgb.$date"
-     ./get_inv.pl "${url}.inv" | grep ":VGRD:" | grep ":10 mb" | \
-     ./get_grib.pl "${url}" pgb.$date
+     ../get_inv.pl "${url}.inv" | grep ":VGRD:" | grep ":10 mb" | \
+     ../get_grib.pl "${url}" pgb.$date
      date=$(($date + 1))
      if [ $(($date % 100)) -eq 13 ] ; then
          date=$(($date - 12 + 100));
