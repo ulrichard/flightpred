ec64f0cddfdb7dc516922a427059e2ff9cd34b25
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2009-10-15 00:05:40 +0000

    started downloading and examine the grib index file
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@22 c5ce1916-e146-4858-a6ec-1f747f202df1

--- src/train/grab_grib.cpp
+++ src/train/grab_grib.cpp
@@ -10,8 +10,11 @@
 #include <boost/date_time/gregorian/gregorian.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/lexical_cast.hpp>
+#include <boost/tokenizer.hpp>
 // standard library
 #include <sstream>
+#include <iomanip>
+
 
 using namespace flightpred;
 using geometry::point_ll_deg;
@@ -60,22 +63,79 @@ void grib_grabber::grab_grib(const bgreg::date &from, const bgreg::date &to, con
         return;
     }
     // download the grib files
-    bfs::path get_gfs_pl(bfs::current_path().getParent "../flightpred/grib/get_gfs.pl");
-    bfs::path gribdir("../flightpred/grib/past");
+    const string baseurl = "http://nomad3.ncep.noaa.gov/pub/reanalysis-2/6hr/pgb/pgb.";
+
+    set<string> sel_levels;
+    sel_levels.insert("surface");
+    sel_levels.insert("850 mb");
+    sel_levels.insert("750 mb");
+    sel_levels.insert("500 mb");
+
+
+    for(bgreg::date mon(from.year(), from.month(), 1); mon <= to; mon += bgreg::months(1))
+    {
+        // first, get the inventory
+        std::stringstream sstr;
+        sstr << "curl " << baseurl << std::setfill('0') << std::setw(4) << static_cast<int>(mon.year())
+             << std::setw(2) << static_cast<int>(mon.month()) << ".inv";
+
+        // ugly C code
+        FILE *fpipe = static_cast<FILE*>(popen(sstr.str().c_str(),"r"));
+        if(!fpipe)
+            throw std::runtime_error("failed to read pipe from downloading grib inventory file.");
+
+        // the line format looks something like :
+        // 14559:182283710:d=2009093006:VGRD:10 mb:anl:NAve=0:range=182283710-182298255
+        char line[1024 * 5];
+        while(fgets(line, sizeof(line), fpipe))
+        {
+            const string strline(line);
+            boost::char_separator<char> sep(":");
+            boost::tokenizer<boost::char_separator<char> > tokens(strline, sep);
+            vector<string> vtokens;
+            std::copy(tokens.begin(), tokens.end(), back_inserter(vtokens));
+            if(vtokens.size() < 8)
+                continue;
+            if(sel_levels.find(vtokens[4]) == sel_levels.end())
+                continue;
+            const string rng = vtokens[7].substr(6, vtokens[7].length());
+            size_t pos = rng.find('-');
+            if(pos = string::npos)
+                continue;
+            size_t rng_start = lexical_cast<size_t>(rng.substr(0, pos-1));
+            size_t rng_end   = lexical_cast<size_t>(rng.substr(pos + 1, rng.length()));
+
+            std::cout << rng_start << " " << rng_end << std::endl;
+
+        }
+        pclose(fpipe);
+
+    }
+
+
+/*
+    // present : http://www.ftp.ncep.noaa.gov/data/nccf/com/gfs/prod
+    const bfs::path project_dir(bfs::current_path().parent_path().parent_path());
+    bfs::path get_gfs_pl(project_dir / "grib/get_gfs.pl");
+    bfs::path gribdir(project_dir / "grib/past");
     if(!bfs::exists(get_gfs_pl))
         throw std::runtime_error("could not locate the get_gfs.pl script");
    if(!bfs::exists(gribdir))
         throw std::runtime_error("could not locate the directory to store the grib files");
     const string var_list = "all"; // or HGT:TMP ...
     const string lev_list = "surface:850_mb:750_mb:500_mb";
+    const set<bpt::time_duration> &runtimes = model_->getPredRunTimes();
     for(bgreg::date day = from; day != to; day += bgreg::days(1))
-    {
-        std::stringstream sstr;
-        sstr << "data " << bgreg::to_iso_string(day) << "00 0 72 3 " << var_list << " " << lev_list
-             << " " << gribdir.external_file_string();
-
-    }
-
+        for(set<bpt::time_duration>::const_iterator rtit = runtimes.begin(); rtit != runtimes.end(); ++rtit)
+        {
+            std::stringstream sstr;
+            sstr << get_gfs_pl.external_file_string() << " data " << bgreg::to_iso_string(day)
+                 << std::setfill('0') << std::setw(2) << rtit->hours() << " 0 72 3 "
+                 << var_list << " " << lev_list << " " << gribdir.external_file_string();
+            const string syscmd(sstr.str());
+            system(syscmd.c_str());
+        }
+*/
     // add the data to the database
 
 }
