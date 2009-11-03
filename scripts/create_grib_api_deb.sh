e12dee97db9ea9dd21c270e7dedb642b0b1e7198
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2009-11-03 20:49:07 +0000

    additional dependencies
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@42 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/create_grib_api_deb.sh
+++ scripts/create_grib_api_deb.sh
@@ -3,6 +3,7 @@
 
 sudo apt-get install libjasper-dev
 sudo apt-get install cdbs dh-buildinfo gfortran flex bison
+sudo apt-get install libjpeg62-dev quilt
 
 cd ../3rd-party/grib_api
 dpkg-buildpackage -rfakeroot
