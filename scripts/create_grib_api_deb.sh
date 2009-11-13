05bd81aecadd017e9e54dcbf25bf5767c6ac3b10
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2009-11-13 18:04:46 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@59 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/create_grib_api_deb.sh
+++ scripts/create_grib_api_deb.sh
@@ -2,7 +2,7 @@
 # compile and install grib_api (maybe create a deb package later)
 
 sudo apt-get install libjasper-dev
-sudo apt-get install cdbs dh-buildinfo gfortran flex bison
+sudo apt-get install cdbs dh-buildinfo gfortran flex bison automake autoconf
 sudo apt-get install libjpeg62-dev quilt
 
 cd ../3rd-party/grib_api
