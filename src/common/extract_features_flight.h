f7bc3f1fc19a7a72433f2f82b48aee137238db52
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2009-11-29 23:28:02 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@76 c5ce1916-e146-4858-a6ec-1f747f202df1

---
+++ src/common/extract_features_flight.h
@@ -0,0 +1,32 @@
+#ifndef EXTRACT_FEATURES_FLIGHT_H_INCLUDED
+#define EXTRACT_FEATURES_FLIGHT_H_INCLUDED
+
+
+//boost
+#include <boost/date_time/gregorian/gregorian_types.hpp>
+#include <boost/noncopyable.hpp>
+// standard library
+#include <string>
+#include <vector>
+
+
+
+namespace flightpred
+{
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8/////////9/////////A
+class feature_extractor_flight : public boost::noncopyable
+{
+public:
+    feature_extractor_flight(const std::string &db_conn_str) : db_conn_str_(db_conn_str) {}
+    virtual ~feature_extractor_flight() {}
+
+    std::vector<double> get_features(size_t pred_site_id, const boost::gregorian::date &day);
+
+private:
+    const std::string db_conn_str_;
+};
+
+/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8/////////9/////////A
+};
+
+#endif // EXTRACT_FEATURES_FLIGHT_H_INCLUDED
