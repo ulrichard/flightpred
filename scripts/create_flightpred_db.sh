a2266973eb1dfb5f72b2be7f4f8fe71c0df4cb0e
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-02-16 01:18:47 +0000

    first version of the debian package that installs without error. It's not finished though.
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@119 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/create_flightpred_db.sh
+++ scripts/create_flightpred_db.sh
@@ -6,8 +6,8 @@ sudo apt-get install postgresql postgresql-contrib postgresql-client postgresql-
 sudo -u postgres dropdb   -e flightpred
 sudo -u postgres createdb -e flightpred
 sudo -u postgres createlang plpgsql flightpred
-sudo -u postgres psql -e -a -d flightpred -f /usr/share/postgresql-8.4-postgis/lwpostgis.sql
-sudo -u postgres psql -e -a -d flightpred -f /usr/share/postgresql-8.4-postgis/spatial_ref_sys.sql
+sudo -u postgres psql -e -a -d flightpred -f /usr/share/postgresql-8.3-postgis/lwpostgis.sql
+sudo -u postgres psql -e -a -d flightpred -f /usr/share/postgresql-8.3-postgis/spatial_ref_sys.sql
 sudo -u postgres psql -e -a -d flightpred -f create_flightpred_db.sql
 
 
