c82fc9395ef3fd6ca37572e5525438e5fa76bf66
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-06-24 22:24:23 +0000

    - building the deb package with a clean svn export to keep the .tar.gz clean of all the temporary and hidden files. - using the required packages for ubuntu lucid.
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@140 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,2 +1,6 @@
-cd ..
+mkdir builddeb
+cd builddeb
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.1
+cd flightpred-0.0.1
 dpkg-buildpackage -rfakeroot
+rm -r flightpred-0.0.1
