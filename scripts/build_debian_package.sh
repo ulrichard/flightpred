8f3a3dbb204101808653045f60e3c45c969f36c2
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-07-29 17:58:13 +0000

    trying to get it into the ppa
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@144 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,6 +1,7 @@
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.1
-cd flightpred-0.0.1
-dpkg-buildpackage -rfakeroot
-rm -r flightpred-0.0.1
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.2
+cd flightpred-0.0.2
+dpkg-buildpackage -rfakeroot -S
+rm -r flightpred-0.0.2
+dput ppa:richi-paraeasy/ppa ./flightpred_0.0.2_source.changes
