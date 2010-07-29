fb5a60dcfc1122f5562b44df51bea42e5e7da9ab
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-07-29 23:43:34 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@147 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,11 +1,11 @@
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.3
-cd flightpred-0.0.3
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.4
+cd flightpred-0.0.4
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flightpred-0.0.3
-dput ppa:richi-paraeasy/ppa ./flightpred_0.0.3_source.changes
+rm -r flightpred-0.0.4
+dput ppa:richi-paraeasy/ppa ./flightpred_0.0.4_source.changes
 cd ..
 rm -r builddeb
 
