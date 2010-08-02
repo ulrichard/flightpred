6f000ff5cad6d6108c67a9d20a0117faa0f4728b
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-08-02 22:04:34 +0000

    adapted debian/postinst to the postgis version used in lucid.
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@149 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,11 +1,10 @@
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.5
-cd flightpred-0.0.5
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.6
+cd flightpred-0.0.6
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flightpred-0.0.5
-dput ppa:richi-paraeasy/ppa ./flightpred_0.0.5_source.changes
+rm -r flightpred-0.0.6
 cd ..
 rm -r builddeb
 
