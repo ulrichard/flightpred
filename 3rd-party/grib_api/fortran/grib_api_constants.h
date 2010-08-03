6632c4d506e201ea4fdf5e9a2421339a79324af9
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-08-03 22:55:00 +0000

    removing unused 3rd party libs: - galib is another genetic algorithms library. I used evocosm and so far it's ok. - grib_api is still used, but in the meantime there is also a package for debian lenny on backports.org
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@152 c5ce1916-e146-4858-a6ec-1f747f202df1

--- 3rd-party/grib_api/fortran/grib_api_constants.h
+++
@@ -1,4 +0,0 @@
-  integer, parameter,public :: GRIB_SUCCESS                        = 0
-  integer, parameter,public :: GRIB_END_OF_FILE                    = -1
-  integer, parameter,public :: GRIB_NULL                           = -1
-  integer, parameter,public :: GRIB_END_OF_INDEX                    = -43
