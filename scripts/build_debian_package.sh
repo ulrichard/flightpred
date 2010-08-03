928cdd0972011611dfe1ba1755e19e5f49234a03
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-08-03 22:16:56 +0000

    trying "apache2ctl restart" in debian/postinst hoping that it won't hang.
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@151 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,10 +1,10 @@
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.6
-cd flightpred-0.0.6
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.7
+cd flightpred-0.0.7
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flightpred-0.0.6
-dput ppa:richi-paraeasy/ppa ./flightpred_0.0.6_source.changes
+rm -r flightpred-0.0.7
+dput ppa:richi-paraeasy/ppa ./flightpred_0.0.7_source.changes
 
