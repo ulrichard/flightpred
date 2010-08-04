3e50f061134af1f281ea7e6f0373aad9ff8a47fa
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-08-04 23:25:22 +0000

    fixed the hanging debian/postinst that troubled a while. I found the solution in the postinst script of the gallery package of which I found a mailing list that indicated that they used to have the same problem.
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@153 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,10 +1,10 @@
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.7
-cd flightpred-0.0.7
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.8
+cd flightpred-0.0.8
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flightpred-0.0.7
-dput ppa:richi-paraeasy/ppa ./flightpred_0.0.7_source.changes
+rm -r flightpred-0.0.8
+dput ppa:richi-paraeasy/ppa ./flightpred_0.0.8_source.changes
 
