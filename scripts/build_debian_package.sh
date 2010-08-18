46f76471547bd791fb4965590f4359e6ab1d8b08
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-08-18 21:40:30 +0000

    v 0.0.10
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@172 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,10 +1,18 @@
+#!/bin/sh
+# build a debian sourcepackage and upload it to the launchpad ppa
+export VersionStr=0.0.10
+export GPGKEY=DA94BB53
+export DEBEMAIL="richi@paraeasy.ch"
+export DEBFULLNAME="Richard Ulrich"
+killall -q gpg-agent
+eval $(gpg-agent --daemon)
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.9
-cd flightpred-0.0.9
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-0.0.10
+cd flightpred-0.0.10
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flightpred-0.0.9
-dput ppa:richi-paraeasy/ppa ./flightpred_0.0.9_source.changes
-
+rm -r flightpred-0.0.10
+dput ppa:richi-paraeasy/ppa ./flightpred_0.0.10_source.changes
+killall -q gpg-agent
