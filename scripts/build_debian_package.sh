65a464103f9aa3a3b44688c5b3687ecab92923c4
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-08-19 21:15:45 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@174 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # build a debian sourcepackage and upload it to the launchpad ppa
-export VersionStr=0.0.11
+FLIGHTPREDVERSIONSTR=0.0.11
 export GPGKEY=DA94BB53
 export DEBEMAIL="richi@paraeasy.ch"
 export DEBFULLNAME="Richard Ulrich"
@@ -9,14 +9,14 @@ eval $(gpg-agent --daemon)
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-$(VersionStr)
-cd flightpred-$(VersionStr)
+svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-$FLIGHTPREDVERSIONSTR
+cd flightpred-$FLIGHTPREDVERSIONSTR
 rm -r 3rd-party/dclib/docs/*
 rm -r 3rd-party/dclib/examples/*
 rm -r 3rd-party/ggl/libs/*
 rm -r 3rd-party/ggl/other/*
 dpkg-buildpackage -rfakeroot -S
 cd ..
-rm -r flightpred-$(VersionStr)
-dput ppa:richi-paraeasy/ppa ./flightpred_$(VersionStr)_source.changes
+rm -r flightpred-$FLIGHTPREDVERSIONSTR
+dput ppa:richi-paraeasy/ppa ./flightpred_${FLIGHTPREDVERSIONSTR}_source.changes
 killall -q gpg-agent
