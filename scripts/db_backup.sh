1a1019893d7ca17c5d33c28d01af105312fb6abb
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-08-23 18:25:35 +0000

    * removing database and user upon purge * only include blobs for trained_solutions table on db backup
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@181 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/db_backup.sh
+++ scripts/db_backup.sh
@@ -1,15 +1,15 @@
 mkdir ../backup
 #sudo -u postgres pg_dump                              flightpred > ../backup/dbdump.sql
 
-sudo -u postgres pg_dump -a -b -t pred_sites          flightpred > ../backup/pred_sites.sql
-sudo -u postgres pg_dump -a -b -t contests            flightpred > ../backup/contests.sql
-sudo -u postgres pg_dump -a -b -t pilots              flightpred > ../backup/pilots.sql
-sudo -u postgres pg_dump -a -b -t sites               flightpred > ../backup/sites.sql
-sudo -u postgres pg_dump -a -b -t flights             flightpred > ../backup/flights.sql
+sudo -u postgres pg_dump -a -t pred_sites          flightpred > ../backup/pred_sites.sql
+sudo -u postgres pg_dump -a -t contests            flightpred > ../backup/contests.sql
+sudo -u postgres pg_dump -a -t pilots              flightpred > ../backup/pilots.sql
+sudo -u postgres pg_dump -a -t sites               flightpred > ../backup/sites.sql
+sudo -u postgres pg_dump -a -t flights             flightpred > ../backup/flights.sql
 sudo -u postgres pg_dump -a -b -t trained_solutions   flightpred > ../backup/trained_solutions.sql
-sudo -u postgres pg_dump -a -b -t weather_models      flightpred > ../backup/weather_models.sql
-sudo -u postgres pg_dump -a -b -t weather_pred        flightpred > ../backup/weather_pred.sql
-sudo -u postgres pg_dump -a -b -t weather_pred_future flightpred > ../backup/weather_pred_future.sql
-sudo -u postgres pg_dump -a -b -t pred_ignore		  flightpred > ../backup/pred_ignore.sql
-sudo -u postgres pg_dump -a -b -t flight_pred         flightpred > ../backup/flight_pred.sql
+sudo -u postgres pg_dump -a -t weather_models      flightpred > ../backup/weather_models.sql
+sudo -u postgres pg_dump -a -t weather_pred        flightpred > ../backup/weather_pred.sql
+sudo -u postgres pg_dump -a -t weather_pred_future flightpred > ../backup/weather_pred_future.sql
+sudo -u postgres pg_dump -a -t pred_ignore		  flightpred > ../backup/pred_ignore.sql
+sudo -u postgres pg_dump -a -t flight_pred         flightpred > ../backup/flight_pred.sql
 
