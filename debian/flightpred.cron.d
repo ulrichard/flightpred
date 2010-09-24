6814b7f57a6a177782e504488a987685378d28db
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-09-24 19:51:03 +0000

    aaarg, that was why cron didn't start the forecast!
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@207 c5ce1916-e146-4858-a6ec-1f747f202df1

--- debian/flightpred.cron.d
+++ debian/flightpred.cron.d
@@ -4,4 +4,4 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 MAILTO=root
 
-0 4 * * * flightpred flightpred-train --db-user flightpred --db-password flightpred --get-future-weather --forecast --db-maintenance
+0 4,15 * * * root /usr/bin/flightpred_train --db-user flightpred --db-password flightpred --get-future-weather --forecast --db-maintenance
