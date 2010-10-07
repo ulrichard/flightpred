a7d879a6a86df8e2356a7809406f190d83c8a549
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-10-07 23:50:48 +0000

    Started implementing an android client app to display the flightpred results.
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@218 c5ce1916-e146-4858-a6ec-1f747f202df1

---
+++ android/flightpred/src/ch/ulrichard/flightpred/XmlHandler.java
@@ -0,0 +1,62 @@
+package ch.ulrichard.flightpred;
+
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.net.*;
+import android.widget.*;
+
+public class XmlHandler {
+	
+	final URL   xmlurl_;
+	
+	public XmlHandler(String xmlUrl) {
+		try{
+			this.xmlurl_ = new URL(xmlUrl);
+		}
+		catch(MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public void update(TableLayout table) {
+		try {
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document dom = builder.parse(xmlurl_.openConnection().getInputStream());
+			Element root = dom.getDocumentElement();
+			NodeList daytags = root.getElementsByTagName("day");
+			Node today = daytags.item(0);
+			NodeList sites = today.getChildNodes();
+			for(int i=0; i<sites.getLength(); ++i) {
+				TableRow row = new TableRow(table.getContext());
+				if(!(sites.item(i) instanceof Element))
+					continue;
+				Element site = (Element)sites.item(i);
+				
+				String name = site.getAttribute("name");
+				TextView txname = new TextView(table.getContext());
+				txname.setText(name);
+				row.addView(txname);
+				
+				NodeList predvalues = site.getChildNodes();
+				for(int j=0; j<predvalues.getLength(); j++) {
+					Node nodval = predvalues.item(j);
+					String nam = nodval.getNodeName();
+					
+					TextView txv = new TextView(table.getContext());
+					txv.setText(nodval.getNodeValue());
+					
+					if(nam.equalsIgnoreCase("num_flight"))
+						row.addView(txv, 1);
+					else if(nam.equalsIgnoreCase("max_dist"))
+						row.addView(txv, 2);
+				} // for predvalues
+					  
+				table.addView(row);
+			} // for sites
+			
+		} catch(Exception e) {
+			throw new RuntimeException(e);		
+		}
+	}
+}
---
+++ android/flightpred/src/ch/ulrichard/flightpred/sitetable.java
@@ -0,0 +1,31 @@
+package ch.ulrichard.flightpred;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.*;
+//import ch.ulrichard.flightpred.*;
+
+public class sitetable extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        try {
+        	loadPredDay();
+        } catch (RuntimeException e) {
+        	String msg = e.getLocalizedMessage();
+        	Log.i("flightpred", msg);
+        }
+    }
+    
+    public void loadPredDay() {
+    	XmlHandler xmlh = new XmlHandler("http://192.168.2.5/flightpred/forecast.php");
+//    	XmlHandler xmlh = new XmlHandler("http://flightpred.homelinux.org/forecast.php");
+    	TableLayout table = (TableLayout)findViewById(R.id.TableLayout01);
+    	xmlh.update(table);
+    }
+    
+}
