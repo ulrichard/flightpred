d68852b8dbc42b2f01978b5b35736b37b4795eef
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-12-21 21:40:15 +0000

    added some packages that are required to build
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@255 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/install_required_deb_ubuntu.sh
+++ scripts/install_required_deb_ubuntu.sh
@@ -3,8 +3,8 @@
 
 sudo apt-get install postgresql-8.4 postgresql-contrib-8.4 postgresql-client-8.4 postgresql-server-dev-8.4 postgresql-8.4-postgis postgis proj libpqxx-dev libgrib-api-dev
 #sudo apt-get install libboost1.37-dev libboost-program-options1.37-dev libboost-date-time1.37-dev libboost-regex1.37-dev libboost-filesystem1.37-dev libboost-program-options1.37-dev
-sudo apt-get install libboost-dev libboost-program-options-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-program-options-dev
+sudo apt-get install libboost-dev libboost-program-options-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev libboost-test-dev
 #sudo apt-get install liblog4cpp5-dev libreadline5-dev libjasper1
-sudo apt-get install libcoyotl-dev libevocosm-dev
+sudo apt-get install libcoyotl-dev libevocosm-dev debhelper
 sudo apt-get install libwt-dev libwthttp-dev libwtext-dev libwtfcgi-dev
 
