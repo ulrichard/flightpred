010fdf98d96f9fe860f13f61afa57199316aad8e
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-12-21 21:46:38 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@256 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/create_flightpred_db.sh
+++ scripts/create_flightpred_db.sh
@@ -9,12 +9,12 @@ fi
 
 sudo -u postgres psql -e -a -c "DROP DATABASE IF EXISTS flightpred"
 sudo -u postgres psql -e -a -c "CREATE USER flightpred WITH PASSWORD 'flightpred' SUPERUSER"
-sudo -u postgres createdb -e flightpred
-sudo -u postgres createlang plpgsql flightpred
-sudo -u postgres psql -e -a -d flightpred -f /usr/share/postgresql/8.4/contrib/postgis.sql
-sudo -u postgres psql -e -a -d flightpred -f /usr/share/postgresql/8.4/contrib/spatial_ref_sys.sql
-sudo -u postgres psql -e -a -d flightpred -f create_flightpred_db.sql
-#sudo -u postgres psql -e -a -c "ALTER ROLE flightpred NOSUPERUSER"
+sudo -u flightpred createdb -e flightpred
+sudo -u flightpred createlang plpgsql flightpred
+sudo -u flightpred psql -e -a -d flightpred -f /usr/share/postgresql/8.4/contrib/postgis.sql
+sudo -u flightpred psql -e -a -d flightpred -f /usr/share/postgresql/8.4/contrib/spatial_ref_sys.sql
+sudo -u flightpred psql -e -a -d flightpred -f create_flightpred_db.sql
+sudo -u postgres psql -e -a -c "ALTER ROLE flightpred NOSUPERUSER"
 
 #easier handling for the current user
 sudo -u postgres createuser --superuser $USER
--- scripts/install_required_deb_ubuntu.sh
+++ scripts/install_required_deb_ubuntu.sh
@@ -2,7 +2,6 @@
 # install required debian packages for ubuntu
 
 sudo apt-get install postgresql-8.4 postgresql-contrib-8.4 postgresql-client-8.4 postgresql-server-dev-8.4 postgresql-8.4-postgis postgis proj libpqxx-dev libgrib-api-dev
-#sudo apt-get install libboost1.37-dev libboost-program-options1.37-dev libboost-date-time1.37-dev libboost-regex1.37-dev libboost-filesystem1.37-dev libboost-program-options1.37-dev
 sudo apt-get install libboost-dev libboost-program-options-dev libboost-date-time-dev libboost-regex-dev libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev libboost-test-dev
 #sudo apt-get install liblog4cpp5-dev libreadline5-dev libjasper1
 sudo apt-get install libcoyotl-dev libevocosm-dev debhelper
