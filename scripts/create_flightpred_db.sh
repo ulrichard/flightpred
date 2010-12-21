1bdca3973f2c032871cfc9d2ca65b38aefa57b65
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2010-12-21 22:46:42 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@257 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/create_flightpred_db.sh
+++ scripts/create_flightpred_db.sh
@@ -4,11 +4,12 @@
 sudo apt-get install postgresql postgresql-contrib postgresql-client postgresql-server-dev-8.4 postgis proj libpqxx-dev
 
 if ! grep -q "^flightpred:" /etc/passwd ; then
-    adduser --gecos 0 --disabled-password --disabled-login flightpred 
+    sudo adduser --gecos 0 --disabled-password --disabled-login flightpred 
 fi
 
 sudo -u postgres psql -e -a -c "DROP DATABASE IF EXISTS flightpred"
 sudo -u postgres psql -e -a -c "CREATE USER flightpred WITH PASSWORD 'flightpred' SUPERUSER"
+sudo -u postgres psql -e -a -c "ALTER ROLE flightpred SUPERUSER"
 sudo -u flightpred createdb -e flightpred
 sudo -u flightpred createlang plpgsql flightpred
 sudo -u flightpred psql -e -a -d flightpred -f /usr/share/postgresql/8.4/contrib/postgis.sql
