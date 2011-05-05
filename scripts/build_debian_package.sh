bfabd107206ef4b03ed4f4adf4b432fabe93e635
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2011-05-05 21:57:00 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@298 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # build a debian sourcepackage and upload it to the launchpad ppa
-FLIGHTPREDVERSIONSTR=0.0.17~maverick
+FLIGHTPREDVERSIONSTR=0.0.17~natty
 export GPGKEY=DA94BB53
 export DEBEMAIL="richi@paraeasy.ch"
 export DEBFULLNAME="Richard Ulrich"
@@ -17,9 +17,10 @@ rm -r 3rd-party/ggl/libs/*
 rm -r 3rd-party/ggl/other/*
 if [ ! -e /etc/dpkg/origins/default ] ; then
     # we're probably on lenny. Substitute some variables and make a binary package
-	dpkg-buildpackage -rfakeroot -d # injecting different package dependencies after the check
+	dpkg-buildpackage -rfakeroot -d -S # injecting different package dependencies after the check
 	cd ..
 	rm -r flightpred-$FLIGHTPREDVERSIONSTR
+	dput ppa:richi-paraeasy/ppa ./flightpred_${FLIGHTPREDVERSIONSTR}_source.changes
 elif  dpkg-vendor --derives-from Ubuntu  ; then
     # we're on ubuntu. Make a source package and upload to the launchpad ppa
 	dpkg-buildpackage -rfakeroot -S
