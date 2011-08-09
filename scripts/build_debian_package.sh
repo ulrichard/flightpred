8def001d40bcecf199f1becc06662ae3221e5266
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2011-08-09 21:54:51 +0000

    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@318 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 # build a debian sourcepackage and upload it to the launchpad ppa
-FLIGHTPREDVERSIONSTR=0.0.28~natty
 export GPGKEY=DA94BB53
 export DEBEMAIL="richi@paraeasy.ch"
 export DEBFULLNAME="Richard Ulrich"
@@ -9,14 +8,20 @@ eval $(gpg-agent --daemon)
 rm -r builddeb
 mkdir builddeb
 cd builddeb
-svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-$FLIGHTPREDVERSIONSTR
-cd flightpred-$FLIGHTPREDVERSIONSTR
-rm -r 3rd-party/dclib/docs/*
-rm -r 3rd-party/dclib/examples/*
-rm -r 3rd-party/ggl/libs/*
-rm -r 3rd-party/ggl/other/*
-dpkg-buildpackage -rfakeroot -S
-cd ..
-rm -r flightpred-$FLIGHTPREDVERSIONSTR
-dput ppa:richi-paraeasy/ppa ./flightpred_${FLIGHTPREDVERSIONSTR}_source.changes
+for DISTRIBUTION in maverick natty oneiric
+do
+	FLIGHTPREDVERSIONSTR=0.0.28~${DISTRIBUTION}
+	svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-$FLIGHTPREDVERSIONSTR
+	cd flightpred-$FLIGHTPREDVERSIONSTR
+	sed -i  -e "s/jaunty/${DISTRIBUTION}/" -e "s/karmic/${DISTRIBUTION}/" -e "s/maverick/${DISTRIBUTION}/" -e "s/natty/${DISTRIBUTION}/" -e "s/oneiric/${DISTRIBUTION}/" debian/changelog
+	sed -i  -e "s/jaunty/${DISTRIBUTION}/" -e "s/karmic/${DISTRIBUTION}/" -e "s/maverick/${DISTRIBUTION}/" -e "s/natty/${DISTRIBUTION}/" -e "s/oneiric/${DISTRIBUTION}/" debian/changelog
+	rm -r 3rd-party/dclib/docs/*
+	rm -r 3rd-party/dclib/examples/*
+	rm -r 3rd-party/ggl/libs/*
+	rm -r 3rd-party/ggl/other/*
+	dpkg-buildpackage -rfakeroot -S
+	cd ..
+	rm -r flightpred-$FLIGHTPREDVERSIONSTR
+	dput ppa:richi-paraeasy/ppa ./flightpred_${FLIGHTPREDVERSIONSTR}_source.changes
+done
 killall -q gpg-agent
