b6d38237f9970ef36da42c90497af55e3fefedcf
ulrichard <ulrichard@c5ce1916-e146-4858-a6ec-1f747f202df1>
2011-11-04 21:29:59 +0000

    * version 0.0.31 *defining the fcgi parameters only once
    git-svn-id: svn://svn.code.sf.net/p/flightpred/code/trunk@325 c5ce1916-e146-4858-a6ec-1f747f202df1

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -10,7 +10,7 @@ mkdir builddeb
 cd builddeb
 for DISTRIBUTION in oneiric # natty maverick
 do
-	FLIGHTPREDVERSIONSTR=0.0.30~${DISTRIBUTION}
+	FLIGHTPREDVERSIONSTR=0.0.31~${DISTRIBUTION}
 	svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-$FLIGHTPREDVERSIONSTR
 	cd flightpred-$FLIGHTPREDVERSIONSTR
 	sed -i  -e "s/maverick/${DISTRIBUTION}/g" -e "s/natty/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" debian/changelog
