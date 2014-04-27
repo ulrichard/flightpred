e0324e4d5a305de7174d9f83c389b2e2555f1a23
Richard Ulrich <richi@paraeasy.ch>
2014-04-27 22:48:01 +0200

    incremented version number

--- scripts/build_debian_package.sh
+++ scripts/build_debian_package.sh
@@ -5,7 +5,7 @@ mkdir builddeb
 cd builddeb
 for DISTRIBUTION in raring # precise oneiric # natty maverick 
 do
-	FLIGHTPREDVERSIONSTR=0.0.39~${DISTRIBUTION}
+	FLIGHTPREDVERSIONSTR=0.0.40~${DISTRIBUTION}
 	svn export https://flightpred.svn.sourceforge.net/svnroot/flightpred/trunk flightpred-$FLIGHTPREDVERSIONSTR
 	cd flightpred-$FLIGHTPREDVERSIONSTR
 	sed -i  -e "s/raring/${DISTRIBUTION}/g" -e "s/precise/${DISTRIBUTION}/g" -e "s/oneiric/${DISTRIBUTION}/g" debian/changelog
